b765813afcad48dd5c054f193fca3757b1850855
Omega_The_III <jeroen.officier@acda.nl>
2021-11-29 16:57:03 +0100

    temp

--- Animal Escape 3D/Assets/Scripts/CameraMotionScript.cs
+++ Animal Escape 3D/Assets/Scripts/CameraMotionScript.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CameraMotionScript : MonoBehaviour
 {
     [SerializeField] private Transform GameFinnishTarget;
-    //[SerializeField] private Transform playerTransform;
-    //[SerializeField] private Vector3 offset;
-    
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
+    private void Start()
+    {
+        offset = new Vector3(player.position.x, player.position.y + 8.0f, player.position.z + 7.0f);
+    }
     private void FixedUpdate()
     {
         //Make sure the camera is always above the player
