798fa049047c28a4765ec386c12a3d52a7ea1739
Omega_The_III <jeroen.officier@acda.nl>
2021-11-29 16:34:13 +0100

    ah

--- Animal Escape 3D/Assets/Scripts/CameraMotionScript.cs
+++ Animal Escape 3D/Assets/Scripts/CameraMotionScript.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CameraMotionScript : MonoBehaviour
 {
     [SerializeField] private Transform GameFinnishTarget;
-    [SerializeField] private Transform playerTransform;
-    [SerializeField] private Vector3 offset;
+    //[SerializeField] private Transform playerTransform;
+    //[SerializeField] private Vector3 offset;
     
     private void FixedUpdate()
     {
         //Make sure the camera is always above the player
-        Vector3 desiredPosition = playerTransform.position + Vector3.up * offset.y;
-        transform.position = desiredPosition;
+        //Vector3 desiredPosition = playerTransform.position + Vector3.up * offset.y;
+        //transform.position = desiredPosition;
 
         //Make sure the player is always looking at the level finnish
         transform.LookAt(GameFinnishTarget.position);
