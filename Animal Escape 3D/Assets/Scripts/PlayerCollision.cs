dc65c607268b758f5317ebf58696513653afad73
OmarOH <36475683+OmarOH@users.noreply.github.com>
2021-12-02 16:43:23 +0100

    Pig collision particles with keeper

---
+++ Animal Escape 3D/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem keeperParticles;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Finish"))
+        {
+            ParticleSystem particles = Instantiate(keeperParticles, gameObject.transform.position, Quaternion.identity);
+            StartCoroutine(DestroyAfterSeconds(particles.gameObject, 1f));
+        }
+    }
+
+    IEnumerator DestroyAfterSeconds(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(obj);
+    }
+}
