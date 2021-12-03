420ac983c8cabe6ca173c07e766c3828b79ec0a5
bossie911 <62554007+bossie911@users.noreply.github.com>
2021-12-03 09:58:27 +0100

    piggie lerp to net

--- Animal Escape 3D/Assets/Scripts/Net.cs
+++ Animal Escape 3D/Assets/Scripts/Net.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class Net : MonoBehaviour
 {
+
+    float timeElapsed;
+    float lerpDuration = 0.5f;
+    bool lerp = false;
+    Vector3 startPos;
+
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeElapsed < lerpDuration && lerp)
+        {
+            player.transform.localPosition = Vector3.Lerp(startPos, new Vector3(0,0,0), timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            other.transform.parent = transform;
-            other.transform.position = transform.position;
+            other.transform.parent = transform;           
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            startPos = other.gameObject.transform.localPosition;
+            lerp = true;
         }
     }
 }
