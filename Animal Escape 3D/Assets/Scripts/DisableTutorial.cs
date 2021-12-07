d5d7aa6a61a6079d5400d8cdb77a000c5e66f72d
bossie911 <62554007+bossie911@users.noreply.github.com>
2021-12-07 14:14:22 +0100

    second tutorial UI

---
+++ Animal Escape 3D/Assets/Scripts/DisableTutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTutorial : MonoBehaviour
+{
+    public GameObject secondTutorialUI;
+    public int id = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(DisableUI());
+        }
+    }
+
+    IEnumerator DisableUI()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+        if (id == 1)
+        {
+            secondTutorialUI.SetActive(true);
+        }
+    }
+}
