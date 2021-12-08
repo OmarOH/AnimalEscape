7598947e34e67d9ced6b6244f45d2b89cb705df1
Omega_The_III <jeroen.officier@acda.nl>
2021-12-08 15:12:49 +0100

    a

---
+++ Animal Escape 3D/Assets/Scripts/GameStates/GameStateCameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GameStateCameraScript : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private Transform newPos;
+
+    void Start()
+    {
+        GameEvents.current.finishTrigger += GameWon;
+    }
+
+    private void GameWon()
+    {
+        virtualCamera.Follow = null;
+    }
+}
