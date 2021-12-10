f43ce42a5ebe78e2e6e722bd7d333828f01784d2
Omega_The_III <jeroen.officier@acda.nl>
2021-12-10 17:04:39 +0100

    a

--- Animal Escape 3D/Assets/Scripts/WaterScript.cs
+++ Animal Escape 3D/Assets/Scripts/WaterScript.cs
@@ -6,10 +6,10 @@ public class WaterScript : MonoBehaviour
 {
     [SerializeField] private ParticleSystem splashEffect;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        Physics.IgnoreCollision(gameObject.GetComponent<BoxCollider>(), collision.collider);
-        if(collision.collider.CompareTag("Player"))
+        Physics.IgnoreCollision(gameObject.GetComponent<BoxCollider>(), collision);
+        if(collision.CompareTag("Player"))
         {
             ParticleSystem particles = Instantiate(splashEffect, new Vector3(collision.transform.position.x,collision.transform.position.y,collision.transform.position.z), Quaternion.identity);
             StartCoroutine(DestroyAfterSeconds(particles.gameObject, 1.5f));
