adbc94f174bdb3a4da61f82d93e3150c3fcafb96
Ghanshaam <gsewnath@gmail.com>
2021-12-13 13:58:43 +0100

    Updated splash

--- Animal Escape 3D/Assets/Scripts/WaterScript.cs
+++ Animal Escape 3D/Assets/Scripts/WaterScript.cs
@@ -12,7 +12,7 @@ public class WaterScript : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             ParticleSystem particles = Instantiate(splashEffect, new Vector3(collision.transform.position.x,collision.transform.position.y,collision.transform.position.z), Quaternion.identity);
-            StartCoroutine(DestroyAfterSeconds(particles.gameObject, 1.5f));
+            StartCoroutine(DestroyAfterSeconds(particles.gameObject, 2f));
             GameEvents.current.GameOver();
         }
     }
